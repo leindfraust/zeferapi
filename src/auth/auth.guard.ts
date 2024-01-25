d7d028c793f96af7210a1b44503731ec7881a2fb
Ronan <rozer223@gmail.com>
2024-01-25 21:47:21 +0800

    refactor: implement AuthGuard separately

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { PostService } from 'src/post/post.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private postService: PostService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers.authorization;
+
+    const token = authorization ? authorization.split(' ')[1] : '';
+    if (!token) {
+      throw new HttpException('Empty API Key', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.validateApiKey(token, request);
+  }
+
+  async validateApiKey(token: string, request: any): Promise<boolean> {
+    const validateUser = await this.postService.validateApiKey({ key: token });
+    if (validateUser) {
+      request.sub = token;
+      request.user = validateUser;
+      return true;
+    }
+    throw new HttpException('Incorrect API Key', HttpStatus.FORBIDDEN);
+  }
+}
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,33 +1,15 @@
-import {
-  Controller,
-  Get,
-  Headers,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { Post } from '@prisma/client';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
-  async getPosts(
-    @Headers('authorization') authorization: string,
-  ): Promise<Post[] | null> {
-    const token = authorization ? authorization.split(' ')[1] : '';
-    let userId: string;
-    if (token) {
-      const validateUser = await this.postService.validateApiKey({
-        key: token,
-      });
-      if (validateUser) {
-        userId = validateUser;
-        return this.postService.posts({ id: userId }, token);
-      }
-      throw new HttpException('Incorrect API Key', HttpStatus.FORBIDDEN);
-    }
-    throw new HttpException('Empty API Key', HttpStatus.BAD_REQUEST);
+  @UseGuards(AuthGuard)
+  async getPosts(@Request() req: any): Promise<Post[] | null> {
+    return this.postService.posts({ id: req.user }, req.sub);
   }
 }
