83f00dc0c644853fe8a177a820ece5963ddd2729
Ronan <rozer223@gmail.com>
2024-01-31 23:06:29 +0800

    feat: has search, cursor, limit and orderBy params
    Now supports multiple query parameters
    
    q for search
    cursor for cursor pagination
    limit for take query prisma
    orderBy accepting only 2 choices, 'most-popular' or 'latest' defaults to 'latest' if orderBy param invalid

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { Post } from '@prisma/client';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -9,7 +16,37 @@ export class PostController {
 
   @Get()
   @UseGuards(AuthGuard)
-  async getPosts(@Request() req: any): Promise<Post[] | null> {
-    return this.postService.posts({ id: req.user }, req.sub);
+  async getPosts(
+    @Request() req: any,
+    @Query() query: any,
+  ): Promise<Post[] | null> {
+    const { cursor, limit, q, orderBy } = query;
+    return this.postService.posts(
+      { id: req.user },
+      req.sub,
+      q,
+      cursor,
+      limit,
+      orderBy,
+    );
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard)
+  async getPost(@Request() req: any, @Param('id') id: string): Promise<Post> {
+    return this.postService.post(
+      { id: req.user },
+      {
+        OR: [
+          {
+            id: id,
+          },
+          {
+            titleId: id,
+          },
+        ],
+      },
+      req.sub,
+    );
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -6,42 +6,109 @@ import { Post, Prisma } from '@prisma/client';
 export class PostService {
   constructor(private prisma: PrismaService) {}
 
+  async updateApiKey<T extends { id?: string; post: Post[] }>(
+    userPosts: T,
+    token: string,
+  ) {
+    await this.prisma.apiKey.update({
+      where: {
+        ownerId: userPosts.id,
+        key: token,
+      },
+      data: {
+        lastUsed: new Date(),
+      },
+    });
+    await this.prisma.apiKeyRequest.create({
+      data: {
+        apiKey: {
+          connect: {
+            key: token,
+          },
+        },
+        requestSize: userPosts.post.length,
+      },
+    });
+  }
+
   async posts(
     userWhereUniqueInput: Prisma.UserWhereUniqueInput,
     token: string,
+    q?: string,
+    cursor?: string,
+    limit?: number,
+    orderBy?: 'most-popular' | 'latest',
   ): Promise<Post[]> {
+    const keyword = q?.split(' ').join('&');
     const userPosts = await this.prisma.user.findUnique({
       where: userWhereUniqueInput,
       select: {
         id: true,
         post: {
           where: {
+            ...(q && {
+              title: {
+                search: keyword,
+              },
+              description: {
+                search: keyword,
+              },
+              author: {
+                search: keyword,
+              },
+            }),
             published: true,
           },
+          ...(cursor && {
+            cursor: {
+              id: cursor,
+            },
+            skip: 1,
+          }),
+          ...(limit && {
+            take: Number(limit),
+          }),
+          ...(orderBy && orderBy === 'most-popular'
+            ? {
+                orderBy: {
+                  views: {
+                    _count: 'desc',
+                  },
+                },
+              }
+            : {
+                orderBy: {
+                  createdAt: 'desc',
+                },
+              }),
         },
       },
     });
     if (userPosts) {
-      await this.prisma.apiKey.update({
-        where: {
-          ownerId: userPosts.id,
-          key: token,
-        },
-        data: {
-          lastUsed: new Date(),
-        },
-      });
-      await this.prisma.apiKeyRequest.create({
-        data: {
-          apiKey: {
-            connect: {
-              key: token,
-            },
-          },
-          requestSize: userPosts.post.length,
-        },
-      });
+      await this.updateApiKey(userPosts, token);
       return userPosts.post;
+    } else {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async post(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    postWhereUniqueInput: Prisma.PostWhereInput,
+    token: string,
+  ): Promise<Post> {
+    const userPost = await this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+      select: {
+        id: true,
+        post: {
+          where: postWhereUniqueInput,
+        },
+      },
+    });
+    if (userPost) {
+      await this.updateApiKey(userPost, token);
+      return userPost.post[0];
     } else {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     }
